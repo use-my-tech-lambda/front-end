d0ddf46eeb21b2b2cf17b77e032326756b6cb803
Fernando Velasco <fervelgo@gmail.com>
2021-06-21 18:10:27 -0500

    login page with ternary working, useHistory pending

---
+++ use-my-tech/src/Cards.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Cards (props) {
+
+
+    return(
+        <div> Cards
+
+
+
+        </div>
+    )
+}
---
+++ use-my-tech/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+export default function Home (props) {
+
+
+    return(
+        <div> Home Page
+
+        </div>
+    )
+}
